b9b4ae3933af267116bc3ddfd54e363315fcf00a
Matthias Tafelmeier <matthias.tafelmeier@gmx.net>
2019-06-16 14:08:19 +0200

    tagcloud streamline

--- blog/pelicanconf.py
+++ blog/pelicanconf.py
@@ -5,21 +5,23 @@ from __future__ import unicode_literals
 THEME = "Responsive-Pelican"
 
 AUTHOR = u'Matthias Tafelmeier'
-SITENAME = "cherusk Tech Blog"
+SITENAME = "Cherusk Tech Meanderings"
 SITE_LOGO = "/meta/cherusk.png"
 SITEURL = 'http://localhost:8000/'
+# SITEURL = ''
+# FAVICON = "meta/favicon.ico"
 
 PATH = 'content'
 
 DEFAULT_LANG = u'en'
 
 # Blogroll
-LINKS = (('github', 'http://github.com/cherusk'),)
+# LINKS = (('index', 'http://github.com/cherusk'),)
 
 STATIC_PATHS = ['meta']
 
 PLUGIN_PATHS = ['/home/matthias/Projects/pelican-plugins/']
-PLUGINS = ['pelican-toc']
+PLUGINS = ['pelican-toc', 'tag_cloud']
 TOC = {
         'TOC_HEADERS'       : '^h[1-7]', # What headers should be included in
        # Expected format is a regular expression
@@ -35,8 +37,14 @@ DISPLAY_PAGES_ON_MENU = True
 DISPLAY_CATEGORIES_ON_MENU = True
 MENUITEMS = [('Contact', SITEURL + '/pages/contact.html')]
 
+TAG_CLOUD_STEPS = 6
+TAG_CLOUD_MAX_ITEMS = 200
+TAG_CLOUD_SORTING = 'size-rev'
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+GITHUB_USER = "cherusk"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
